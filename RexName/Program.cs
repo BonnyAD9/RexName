ed1daf9024c44e4babd70539aa9127aa9c30a57d
Jakub A. Štigler <jakubstig@gmail.com>
2022-02-07 11:52:38 +0100

    Add project files.

---
+++ RexName/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RexName;
+
+class Program
+{
+    static bool NoOp = false;
+    static string SearchPattern = "";
+    static string ReplacePattern = "";
+    static string Directory = "./";
+
+    static void Main(string[] args)
+    {
+        //args = new string[] { @"(.*?)( - Copy)?( \(.*\))?.txt", @"<1> ({i}).txt", @"-noop", @"-d", @"TestFolder" };
+        if (args.Length < 2)
+        {
+            Console.WriteLine("Invalid number of arguments");
+            return;
+        }
+        SearchPattern = args[0];
+        ReplacePattern = args[1];
+        for (int i = 2; i < args.Length; i++)
+        {
+            switch (args[i].ToLower())
+            {
+                case "-noop":
+                    NoOp = true;
+                    break;
+                case "--directory" or "-d":
+                    i++;
+                    Directory = args[i];
+                    break;
+                default:
+                    Console.WriteLine($"Invalid argument '{args[i]}'");
+                    return;
+            }
+        }
+
+        RegexRename rr;
+        try
+        {
+            rr = new(Directory);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Couldn't get files from directory '{Directory}'");
+            Console.WriteLine(ex.Message);
+            return;
+        }
+
+        int index = 0;
+        rr.GetVariable = (name, _) =>
+        {
+            int i = name.IndexOf(':');
+
+            if (i < 0) return name switch
+            {
+                "i" => index++.ToString(),
+                _ => null,
+            };
+
+            return name[..i] switch
+            {
+                "i" => index++.ToString(name[(i + 1)..]),
+                _ => null,
+            };
+        };
+
+        int c;
+        try
+        {
+            c = rr.Match(SearchPattern, ReplacePattern);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Invalid regex pattern");
+            Console.WriteLine(ex.Message);
+            return;
+        }
+
+        if (c < 0)
+        {
+            Console.WriteLine("Replace pattern was incorrect");
+            return;
+        }
+
+        Console.WriteLine($"Found: {c}");
+        if (NoOp)
+        {
+            foreach ((var file, var name) in rr)
+            {
+                Console.WriteLine($"{file.Name} -> {name}");
+            }
+            return;
+        }
+
+        rr.Rename();
+    }
+}
---
+++ RexName/RegexRename.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Dir = System.IO.Directory;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace RexName;
+
+public class RegexRename : IEnumerable<(FileInfo, string)>
+{
+    public string Directory { get; init; }
+    private FileInfo[] Files { get; init; }
+    private List<(FileInfo, string)> NewNames { get; } = new();
+    public Regex? Pattern { get; private set; } = null;
+    public Func<string, string, string?> GetVariable { get; set; } = static (_, _) => null;
+
+    public (FileInfo File, string Name) this[int index] => NewNames[index];
+
+    public RegexRename(string directory, SearchOption searchOption = SearchOption.TopDirectoryOnly)
+    {
+        Directory = directory;
+        
+        var files = Dir.GetFiles(directory, "*", searchOption);
+        Files = new FileInfo[files.Length];
+
+        for (int i = 0; i < files.Length; i++)
+            Files[i] = new(files[i]);
+    }
+
+    public int Match(string matchPattern, string replacePattern)
+    {
+        Regex ex = new(matchPattern);
+        foreach (var file in Files)
+        {
+            var match = ex.Match(file.Name);
+            if (!match.Success)
+                continue;
+            string? name = MakeName(match, replacePattern, file.FullName);
+            if (name is null)
+                return -1;
+            NewNames.Add((file, name!));
+        }
+        
+        return NewNames.Count;
+    }
+
+    public void Rename()
+    {
+        foreach ((var file, var name) in NewNames)
+        {
+            if (file.DirectoryName is null)
+                continue;
+            file.MoveTo(Path.Combine(file.DirectoryName!, name), true);
+        }
+    }
+
+    private string? MakeName(Match match, string pattern, string file)
+    {
+        StringBuilder sb = new();
+
+        for (int i = 0; i < pattern.Length; i++)
+        {
+            int newI = i;
+            string? toAppend = null;
+            switch (pattern[i])
+            {
+                case '<':
+                    i++;
+                    newI = pattern.IndexOf('>', i);
+                    if (newI < 0)
+                        return null;
+                    toAppend = match.Groups[pattern[i..newI]].Value;
+                    break;
+                case '{':
+                    i++;
+                    newI = pattern.IndexOf('}', i);
+                    if (newI < 0)
+                        return null;
+                    toAppend = GetVariable(pattern[i..newI], file);
+                    break;
+                case '\\':
+                    i++;
+                    sb.Append(pattern[i]);
+                    continue;
+                default:
+                    sb.Append(pattern[i]);
+                    continue;
+            }
+
+            if (toAppend is null)
+                return null;
+            sb.Append(toAppend!);
+            
+            i = newI;
+        }
+
+        return sb.ToString();
+    }
+
+    public IEnumerator<(FileInfo, string)> GetEnumerator() => NewNames.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => NewNames.GetEnumerator();
+}

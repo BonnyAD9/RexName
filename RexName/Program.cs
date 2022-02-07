7397b9672ec468d1a0dd697504884f0df446666a
Jakub A. Štigler <jakubstig@gmail.com>
2022-02-07 12:18:39 +0100

    Removed unnecesary usings

--- RexName/Program.cs
+++ RexName/Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Text.RegularExpressions;
-
-namespace RexName;
+﻿namespace RexName;
 
 class Program
 {
--- RexName/RegexRename.cs
+++ RexName/RegexRename.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Text;
 using System.Text.RegularExpressions;
 using Dir = System.IO.Directory;
-using System.Threading.Tasks;
 using System.Collections;
 
 namespace RexName;
